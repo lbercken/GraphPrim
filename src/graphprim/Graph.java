f55e9c924a85b0e587991d7fe1357d296cee5e9a
Laurens van den Bercken <laurens@sol.private.lan>
2015-03-03 11:36:57 +0100

    Added basic functionality to Graph class

--- src/graphprim/Graph.java
+++ src/graphprim/Graph.java
@@ -9,21 +9,51 @@ import java.util.ArrayList;
  */
 public class Graph {
     
+    private int size;
     private ArrayList<Vertex> nodes;
     
-    public Graph() {
+    public Graph(int size) {
         nodes = new ArrayList<>();
-    }
-    
-    public void addVertex(String str) {
-        nodes.add(new Vertex(str));
-    }
-    
-    public void addEdge(Vertex from, Vertex to, int weight) {
-        nodes.
-        if(nodes.contains(from) && nodes.contains(to)) {
-            
+        for(int i = 0; i < size; i++) {
+            nodes.add(new Vertex());
         }
+        this.size = size;
+    }
+    
+    public void addEdge(int from, int to, int weight) {
+        // Precondition
+        if(from >= size || to >= size) {
+            System.out.println("Error: from or to is greater than size."
+                    + "No edge has been added");
+            return;
+        }
+        nodes.get(from).addOutnode(nodes.get(to), weight);
+    }
+    
+    public void deleteEdge(int from, int to) {
+        // Precondition
+        if(from >= size || to >= size) {
+            System.out.println("Error: from or to is greater than size."
+                    + "No edge has been deleted");
+            return;
+        }
+        nodes.get(from).deleteOutnode(nodes.get(to));
+    }
+    
+    public boolean isEdge(int from, int to) {
+        if(from >= size || to >= size) {
+            return false;
+        }
+        return nodes.get(from).getSuccessors().contains(nodes.get(to));
+    }
+    
+    public ArrayList<Vertex> getSuccessors(int vertex) {
+        // Precondition
+        if(vertex >= size) {
+            System.out.println("Error: vertex does not exist.");
+            return null;
+        }
+        return nodes.get(vertex).getSuccessors();
     }
     
 }
--- src/graphprim/Vertex.java
+++ src/graphprim/Vertex.java
@@ -1,6 +1,8 @@
 
 package graphprim;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 /**
@@ -8,16 +10,33 @@ import java.util.Hashtable;
  * @author Laurens van den Bercken
  */
 public class Vertex {
-    
-    private String name;
+   
     private Hashtable<Vertex, Integer> outnodes; // Adjacency list, with weighted edges
     
-    public Vertex(String str) {
-        name = str;
+    public Vertex() {
         outnodes = new Hashtable<>();
     }
     
     public void addOutnode(Vertex node, int weight) {
         outnodes.put(node, weight);
+    }
+    
+    public void deleteOutnode(Vertex node) {
+        // Precondition
+        if (!outnodes.containsKey(node)) {
+            System.out.println("Error: edge does not exist."
+                    + "No edge has been deleted.");
+            return;
+        }
+        outnodes.remove(node);
+    }
+    
+    public ArrayList<Vertex> getSuccessors() {
+        ArrayList<Vertex> ret = new ArrayList<>();
+        Enumeration en = outnodes.keys();
+        while(en.hasMoreElements()) {
+            ret.add((Vertex) en.nextElement());
+        }
+        return ret;
     }
  }
